ce63ab15793e820275476e06972108e266794ea2
sebastien fournier <sfournier.qc@gmail.com>
2023-10-04 09:19:16 -0400

    console new

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+House house1 = new House(2002, 7000);
+Console.WriteLine(house1.CanBeSold(house1.Year));
+
+class House
+{
+  public House (int year, int size)
+  {
+    Year = year;
+    Size = size;
+  }
+
+  public int Year { get; set; }
+  public int Size { get; set; }
+
+  private int HowOld(int year)
+  {
+    return DateTime.Now.Year - year;
+  }
+
+  public bool CanBeSold(int year)
+  {
+    if (HowOld(year) > 15)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+}
